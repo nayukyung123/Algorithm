976438414194bfdca7e672947b1a3654c303672d
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-08-06 19:53:15 +0900

    [Silver V] Title: 학번, Time: 696 ms, Memory: 166356 KB -BaekjoonHub

---
+++ 백준/Silver/3711. 학번/학번.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		for (int k = 0; k < n; k++) {
+			int g = Integer.parseInt(br.readLine());// 학생 수 입력
+			int[] arr = new int[g]; // 각 학생의 학번을 저장할 배열 생성
+			for (int i = 0; i < g; i++) {
+				arr[i] = Integer.parseInt(br.readLine());
+
+			}
+
+			int m = 0; // 답 m 을 찾을 변수 선언
+			while (true) {
+				m++;
+				boolean istrue = true;
+				ArrayList<Integer> modlist = new ArrayList<Integer>(); // 학번을 m으로 나눈 나머지를 저장할 리스트
+				for (int i = 0; i < g; i++) {// 모든 학번을 순회하며 m으로 나눔
+					if (modlist.contains(arr[i] % m)) {// m으로 나눈 나머지가 이미 나온 값인지 판단
+						istrue = false;
+						break;
+					}
+					modlist.add(arr[i] % m);
+
+				}
+				if (istrue == true) {
+					System.out.println(m);
+					break;
+				}
+
+			}
+
+		}
+
+	}
+}
