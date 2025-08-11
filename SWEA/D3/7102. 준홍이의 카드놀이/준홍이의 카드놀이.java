7ecbf4ea93d6f219fe54445e4366418ba44572d0
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-08-11 17:41:37 +0900

    [D3] Title: 준홍이의 카드놀이, Time: 136 ms, Memory: 29,184 KB -BaekjoonHub

---
+++ SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// 두개의 주사위를 돌렸을 때의 합 중 등장할 확률이 높은것...
+		// 합 sum 가 나올 경우의 수
+		// 최빈값
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt(); // 두번째 카드세트
+			int M = sc.nextInt(); // 첫번째 카드세트
+
+			int lo = Math.min(N, M) + 1;// 최빈 합의 시작
+			int hi = Math.max(N, M) + 1;// 최빈 합의 끝
+
+			System.out.print("#" + tc + " ");
+			for (int s = lo; s <= hi; s++) {
+				System.out.print(s + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
