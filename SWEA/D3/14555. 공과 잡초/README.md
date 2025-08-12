d3935eedc140eb3bd9ce8f8a787e73cc743f026a
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-08-13 01:13:12 +0900

    [D3] Title: 공과 잡초, Time: 79 ms, Memory: 25,088 KB -BaekjoonHub

---
+++ SWEA/D3/14555. 공과 잡초/README.md
@@ -0,0 +1,15 @@
+# [D3] 공과 잡초 - 14555 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYGtoa3qARcDFARC) 
+
+### 성능 요약
+
+메모리: 25,088 KB, 시간: 79 ms, 코드길이: 871 Bytes
+
+### 제출 일자
+
+2025-08-13 01:13
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Solution {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String str = br.readLine();
+
+			int result = 0;
+
+			for (int i = 0; i < str.length(); i++) {
+				char c = str.charAt(i);
+				if (c == '(') {
+
+					result++; // 열린 괄호면 공으로 간주
+				} else if (c == ')') {
+                    // 현재 문자가 닫는 괄호 ')'일 때
+                    // 이전 문자가 '|'이면 카운트 증가
+                    // (i > 0 조건: 이전 문자가 존재해야 함)
+					if (i > 0 && str.charAt(i - 1) == '|') {
+
+						result++;
+					}
+				}
+
+			}
+			System.out.println("#" + test_case + " " + result);
+
+		}
+	}
+
+}
