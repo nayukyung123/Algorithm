4a58e5209049447f93408b0950cc2f24f216fe60
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-08-13 01:22:22 +0900

    [D3] Title: 공과 잡초, Time: 86 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D3/14555. 공과 잡초/공과 잡초.java
+++ SWEA/D3/14555. 공과 잡초/공과 잡초.java
@@ -13,16 +13,16 @@ class Solution {
 
 			int result = 0;
 
-			for (int i = 0; i < str.length(); i++) {
-				char c = str.charAt(i);
+			for (int index = 0; index < str.length(); index++) {
+				char c = str.charAt(index);
 				if (c == '(') {
 
 					result++; // 열린 괄호면 공으로 간주
 				} else if (c == ')') {
                     // 현재 문자가 닫는 괄호 ')'일 때
                     // 이전 문자가 '|'이면 카운트 증가
-                    // (i > 0 조건: 이전 문자가 존재해야 함)
-					if (i > 0 && str.charAt(i - 1) == '|') {
+                    // (index > 0 조건: 이전 문자가 존재해야 함)
+					if (index > 0 && str.charAt(index - 1) == '|') {
 
 						result++;
 					}
