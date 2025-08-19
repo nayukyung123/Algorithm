1b1a4708adf85abcc1ecae1c8a1bde0e30dcc6e4
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-08-20 00:31:06 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 107 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,23 +1,27 @@
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = Integer.parseInt(sc.nextLine());
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
-			long N = Long.parseLong(sc.nextLine());
-			Set<Character> seen = new HashSet<>();
-			long k = 0;
+			int N = sc.nextInt();
+			Set<Integer> set = new HashSet<>();
 			
-			while (seen.size() < 10) {
-				long x = N * ++k;
-				for (char c : String.valueOf(x).toCharArray()) {
-					seen.add(c);
+			int cnt = 0;
+			int result = 0;
+			
+			while (set.size()<10) {
+				 result = N * ++cnt; // int형 한 글자 씩 빼오는 법 
+				 String s = String.valueOf(result); // int -> String 
+				 for (char c : s.toCharArray()) { // toCharArray 문자열을 문자 하나씩 잘라서 배열에 담아줌
+					set.add(c -'0');
 				}
 			}
-			System.out.println("#" + tc + " " + k*N);
+			System.out.println("#" + tc + " " + result);
 		}
-
 	}
 }
