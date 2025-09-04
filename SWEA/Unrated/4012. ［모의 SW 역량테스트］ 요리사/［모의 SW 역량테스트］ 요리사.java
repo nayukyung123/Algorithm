6b60de761e356e286dd8e16dbdd3b5bed67c775c
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-04 16:55:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 302 ms, Memory: 39,240 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int N;
-	static int[][] S;
-	static boolean[] selected;
-	static int min;
+	static int N; // 식재료 개수
+	static int[][] S; // 시너지 저장 배열
+	static boolean[] selected; // 각 재료가 팀A에 속하는지 여부 표시
+	static int min; // 두 팀 시너지 차이의 최소값
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -15,15 +15,17 @@ public class Solution {
 
 			N = sc.nextInt();
 			S = new int[N][N];
-			selected = new boolean[N];
+			selected = new boolean[N]; // 초기엔 전부 false (팀B)
 			min = Integer.MAX_VALUE;
 
+			// 시너지 값 입력
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-                    S[i][j] = sc.nextInt();
+					S[i][j] = sc.nextInt();
 				}
 			}
 
+			// 팀 나누기
 			combination(0, 0);
 
 			System.out.println("#" + tc + " " + min);
@@ -56,14 +58,16 @@ public class Solution {
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = i + 1; j < N; j++) {
-				if (selected[i] && selected[j]) {
+				if (selected[i] && selected[j]) { // 팀 A
 					A += S[i][j] + S[j][i];
-				}else if (!selected[i] && !selected[j]) {
+				}else if (!selected[i] && !selected[j]) { //팀 B
 					B += S[i][j] + S[j][i];
 				}
 			}
 			
 		}
+		
+		 // 두 팀의 시너지 차이 최소값 갱신 abs:절대값
 		min = Math.min(min, Math.abs(A - B));
 
 	}
