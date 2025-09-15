18fc5a1880d99c984eb20b3bdf038fdca1946d7f
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-15 17:45:05 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 447 ms, Memory: 140,504 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -1,31 +1,36 @@
 import java.util.*;
+import java.io.*;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(); 
+
+	static int day, maxPrice;
+	static long profit; 
+	static int[] priceArr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			int[] price = new int[N];
+			int N = Integer.parseInt(br.readLine());
 
+			priceArr = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
-				price[i] = sc.nextInt();
+				priceArr[i] = Integer.parseInt(st.nextToken());
 			}
 
-			long profit = 0L;
-			int maxRight = 0;
+			maxPrice = 0; 
+			profit = 0;
 
-			for (int i = N -1 ; i >= 0; i--) {
-				if (price[i] > maxRight)
-					maxRight = price[i];
-				else
-					profit += (maxRight - price[i]);
-
+			for (int i = N - 1; i >= 0; i--) {
+				if (priceArr[i] < maxPrice) {
+					profit += maxPrice - priceArr[i];
+				} else {
+					maxPrice = priceArr[i];
+				}
 			}
-
 			System.out.println("#" + tc + " " + profit);
-
 		}
 
 	}
