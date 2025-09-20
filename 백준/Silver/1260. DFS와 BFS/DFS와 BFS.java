08f83f2119e5502d55bd639dd75764d5287566bf
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-20 15:36:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 284 ms, Memory: 19872 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static int N, M, V, a, b;
+	static int N, M, V;
 	static List<Integer>[] graph;
 	static boolean[] visited;
 
@@ -22,55 +21,55 @@ public class Main {
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
 			graph[a].add(b);
 			graph[b].add(a);
 
 		}
+
 		for (int i = 0; i <= N; i++) {
 			Collections.sort(graph[i]);
 		}
+
 		visited = new boolean[N + 1];
 		dfs(V);
-
+		
 		System.out.println();
-
+		
 		visited = new boolean[N + 1];
 		bfs(V);
 
 	}
 
+	private static void bfs(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(v);
+		visited[v] = true;
+		
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			
+			System.out.print(cur + " ");
+			
+			for (int next : graph[cur]) {
+				if(!visited[next]) {
+					visited[next]=true;
+					q.offer(next);
+				}
+			}
+		}
+	}
+
 	private static void dfs(int node) {
 		visited[node] = true;
-
 		System.out.print(node + " ");
 		for (int next : graph[node]) {
 			if (!visited[next]) {
 				dfs(next);
 			}
 		}
-	}
-
-	private static void bfs(int V) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(V);
-		visited[V] = true;
-
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			System.out.print(cur + " ");
-
-			for (int next : graph[cur]) {
-				if (!visited[next]) {
-					visited[next] = true;
-					q.offer(next);
-				}
-			}
-
-		}
 
 	}
-
 }
