d0a8d9c3f24cb94c3c4813e6fb9888c56fb99d44
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-20 18:46:14 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -3,47 +3,45 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M; // 정점, 간선
-	static int a, b;
-	static boolean[] visited;
 	static List<Integer>[] graph;
-	static int count;
+	static int N, M, ans;
+	static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 
 		graph = new ArrayList[N + 1];
-		for (int i = 0; i < N + 1; i++) {
+		for (int i = 0; i <= N; i++) {
 			graph[i] = new ArrayList<>();
 		}
 
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
 			graph[a].add(b);
 			graph[b].add(a);
+
 		}
-
+		
 		visited = new boolean[N+1];
-
-		dfs(1);
-
-		System.out.println(count - 1);
-
+		
+		dfs(1); // 1번 컴퓨터를 통해 웜바이러스가 걸리는 컴퓨터의 수 출력
+		System.out.println(ans-1);
 	}
 
 	private static void dfs(int node) {
 		visited[node] = true;
-		count++;
+		ans++;
 
 		for (int next : graph[node]) {
 			if (!visited[next]) {
 				dfs(next);
 			}
 		}
+
 	}
 }
