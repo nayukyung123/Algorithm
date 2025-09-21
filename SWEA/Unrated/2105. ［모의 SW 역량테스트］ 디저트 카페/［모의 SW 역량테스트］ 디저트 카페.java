358885895df343e30e22fcffa98b6831dda6c916
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-21 17:55:09 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 141 ms, Memory: 28,800 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -3,62 +3,55 @@ import java.util.*;
 
 public class Solution {
 
-	static int N, ans;
-	static int[][] map;
-	static boolean[] dessert;
-	static int[] dr = { 1, 1, -1, -1 };
-	static int[] dc = { 1, -1, -1, 1 };
+    static int N, ans;
+    static int[][] map;
+    static int[] dr = { 1, 1, -1, -1 };
+    static int[] dc = { 1, -1, -1, 1 };
+    static boolean[] dessert;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            N = Integer.parseInt(br.readLine());
+            map = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            ans = -1;
 
-		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			map = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    dessert = new boolean[101];
+                    dessert[map[i][j]] = true;     
+                    dfs(i, j, i, j, 0, 1);        
+                }
+            }
 
-			ans = -1;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					dessert = new boolean[101];
-                    dessert[map[i][j]] = true;
-					dfs(i, j, i, j, 0, 1);
+            System.out.println("#" + tc + " " + ans);
+        }
+    }
 
-				}
+    private static void dfs(int r, int c, int startR, int startC, int dir, int count) {
+        for (int d = dir; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-			}
+            if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
 
-			System.out.println("#" + tc + " " + ans);
-		}
-	}
+            if (nr == startR && nc == startC && d == 3 && count >= 4) {
+                ans = Math.max(ans, count);
+                return;
+            }
 
-	static void dfs(int r, int c, int startR, int startC, int dir, int count) {
-		
-		for (int d = dir; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-
-			if (nr < 0 || nr >= N || nc < 0 || nc >= N)
-				continue;
-
-			if (nr == startR && nc == startC && d == 3 && count >=4) {
-				
-				ans = Math.max(ans, count);
-				return;
-			}
-
-			int type = map[nr][nc];
-			if (!dessert[type]) {
-				dessert[type] = true;
-				dfs(nr, nc, startR, startC, d, count + 1);
-				dessert[type] = false; 
-			}
-		}
-	}
+            if (!dessert[map[nr][nc]]) {
+                dessert[map[nr][nc]] = true;
+                dfs(nr, nc, startR, startC, d, count + 1); 
+                dessert[map[nr][nc]] = false;            
+            }
+        }
+    }
 }
