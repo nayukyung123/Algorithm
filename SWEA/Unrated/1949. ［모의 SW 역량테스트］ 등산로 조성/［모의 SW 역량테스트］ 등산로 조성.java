5dbde148f7586be83ffd38dfd154b2a0d96d6a64
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-21 16:09:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 91 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Solution {
 
-	static int N, K, ans, max;
+	static int N, K, TOP, maxLen;
 	static int[][] map;
 	static boolean[][] visited;
 	static int[] dr = { -1, 1, 0, 0 };
@@ -12,75 +12,59 @@ public class Solution {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-
 		for (int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 
+			TOP = 0;
+			visited = new boolean[N][N];
 			map = new int[N][N];
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
+					TOP = Math.max(TOP, map[i][j]);
 				}
 			}
 
-			max = 0;
+			maxLen = 0;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					max = Math.max(max, map[i][j]);
-
-				}
-			}
-
-			visited = new boolean[N][N];
-			ans = 0;
-
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (map[i][j] == max) {
-						visited[i][j] = true;
+					if (map[i][j] == TOP) {
 						dfs(i, j, 1, false);
-						visited[i][j] = false;
 					}
-
 				}
 			}
-			System.out.println("#" + tc + " " + ans);
-
+			System.out.println("#" + tc + " " + maxLen);
 		}
 	}
 
-	private static void dfs(int r, int c, int len, boolean cutUsed) {
-		ans = Math.max(ans, len);
+	private static void dfs(int i, int j, int len, boolean usedK) {
+		visited[i][j] = true;
+
+		maxLen = Math.max(maxLen, len);
 
 		for (int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+			int nr = i + dr[d];
+			int nc = j + dc[d];
 
-			if (nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) {
-			    continue;
-			}
-
-			if (map[nr][nc] < map[r][c]) {
-				visited[nr][nc] = true;
-				dfs(nr, nc, len + 1, cutUsed);
-				visited[nr][nc] = false;
-			} else if (!cutUsed) {
-				for (int cut = 1; cut <= K; cut++) {
-					if(map[nr][nc] -cut < map[r][c]) {
-						map[nr][nc] -= cut;
-						visited[nr][nc] = true;
-						dfs(nr, nc, len + 1, true);
-						visited[nr][nc] = false;
-						map[nr][nc] += cut;
+			if (nr >= 0 && nc >= 0 && nr < N && nc < N && !visited[nr][nc]) {
+				if(map[nr][nc] < map[i][j]) { 
+					dfs(nr,nc,len+1,usedK);
+				}else {  
+					if(!usedK && map[nr][nc]-K < map[i][j]) {
+						int original = map[nr][nc];
+						map[nr][nc] = map[i][j]-1;
+						dfs(nr,nc,len+1,true);
+						map[nr][nc] =original;
+						
 					}
 				}
 			}
 
 		}
+		visited[i][j] = false;
 
 	}
-
 }
