5f5fb1a4510be37a30231a20de5d39d9e6c8e238
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-21 21:32:02 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 71 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -3,68 +3,59 @@ import java.util.*;
 
 public class Solution {
 
-	static int ans;
-	static int[][] map = new int[16][16];
-	static boolean[][] visited;
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+    static int ans;
+    static int[][] map = new int[16][16];
+    static boolean[][] visited;
+    static int[] dr = { -1, 1, 0, 0 };
+    static int[] dc = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for (int tc = 1; tc <= 10; tc++) {
-			br.readLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			for (int i = 0; i < 16; i++) {
-				String line = br.readLine();
-				for (int j = 0; j < 16; j++) {
-					map[i][j] = line.charAt(j) - '0';
-				}
-			}
+        for (int tc = 1; tc <= 10; tc++) {
+            br.readLine();
 
-			// 갈수있음 1
-			// 갈수없음 0
-			// 전부 가야함
-			ans = 0;
-			visited = new boolean[16][16];
-			for (int i = 0; i < 16; i++) {
-				for (int j = 0; j < 16; j++) {
-					if (map[i][j] == 2) {
-						visited[i][j] = true;
-						bfs(i, j);
-					}
-				}
-			}
-			System.out.println("#" + tc + " " + ans);
+            for (int i = 0; i < 16; i++) {
+                String line = br.readLine();
+                for (int j = 0; j < 16; j++) {
+                    map[i][j] = line.charAt(j) - '0';
+                }
+            }
 
-		}
-	}
+            ans = 0;
+            visited = new boolean[16][16];
 
-	private static void bfs(int i, int j) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] { i, j });
-		visited[i][j] = true;
+            // 시작점(2)에서 DFS 시작
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < 16; j++) {
+                    if (map[i][j] == 2) {
+                        visited[i][j] = true;
+                        dfs(i, j);
+                    }
+                }
+            }
 
-		while (!q.isEmpty()) {
-			int[] cur = q.poll();
-			int r = cur[0];
-			int c = cur[1];
+            System.out.println("#" + tc + " " + ans);
+        }
+    }
 
-			if (map[r][c] == 3) {
-				ans = 1;
-				return;
-			}
+    private static void dfs(int r, int c) {
+        if (ans == 1) return; 
 
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
+        if (map[r][c] == 3) { 
+            ans = 1;
+            return;
+        }
 
-				if (nr < 16 && nc < 16 && nr >= 0 && nc >= 0 && !visited[nr][nc] && map[nr][nc] != 1) {
-					visited[nr][nc] = true;
-					q.offer(new int[] { nr, nc });
+        for (int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-				}
-			}
-		}
-
-	}
+            if (nr >= 0 && nr < 16 && nc >= 0 && nc < 16 
+                    && !visited[nr][nc] && map[nr][nc] != 1) {
+                visited[nr][nc] = true;
+                dfs(nr, nc);
+            }
+        }
+    }
 }
