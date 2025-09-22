52e63668941317dcb4276eef23cd6dd9a63507d9
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-22 23:30:58 +0900

    [D2] Title: 스도쿠 검증, Time: 90 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,58 +1,63 @@
+import java.io.*;
 import java.util.*;
 
-//d
-// 스도쿠 퍼즐의 숫자들이 주어졌을 때,겹치는 숫자가 없을 경우-> 1 ,,   그렇지 않을 경우 0 을 출력한다
-//d1 1 에서 9 까지의 숫자를 한번씩만 넣고, 3 x 3 크기의 작은 격자 또한, 1 에서 9 까지의 숫자가 겹치지 않아야 한다.
-//d
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = Integer.parseInt(sc.nextLine());
 
+	static int[][] map = new int[9][9];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			int[][] map = new int[9][9];
-			boolean valid = true;
-
+			boolean isValid = true;
+			
 			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++)
-					map[i][j] = sc.nextInt();
-			} // 9*9 퍼즐
-
-			for (int i = 0; i < 9; i++) {
-				Set<Integer> set = new HashSet<>();
+				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < 9; j++) {
-					if (!set.add(map[i][j]))
-						valid = false;
-
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
-			} // 행 검사
-
-			for (int j = 0; j < 9; j++) {
-				Set<Integer> set = new HashSet<>();
-				for (int i = 0; i < 9; i++) {
-					if (!set.add(map[i][j]))
-						valid = false;
-
-				}
-			} // 열 검사
-
-			for (int bi = 0; bi < 9; bi += 3) {
-				for (int bj = 0; bj < 9; bj += 3) {
-					Set<Integer> boxSet = new HashSet<>();
-					for (int i = 0; i < 3; i++) {
-						for (int j = 0; j < 3; j++) {
-							int num = map[bi + i][bj + j];
-							if (!boxSet.add(num))
-								valid = false;
-
-						}
+			} // 맵 입력받음
+			
+			
+			// 가로
+			for (int i = 0; i < 9 && isValid; i++) {
+				Set<Integer> row = new HashSet<>();
+				for (int j = 0; j < 9; j++) {
+					int num = map[i][j];
+					if (!row.add(num)) {
+						isValid = false;
+						break;
 					}
 				}
-
-			}// 3*3 검사 
-			
-			int result = valid ? 1 : 0;
-			System.out.println("#" + tc + " " + result);
+			}
+			// 세로
+			for (int j = 0; j < 9 && isValid; j++) {
+				Set<Integer> col = new HashSet<>();
+				for (int i = 0; i < 9; i++) {
+					int num = map[i][j];
+					if (!col.add(num)) {
+						isValid = false;
+						break;
+					}
+				}
+			}
+			// 3 x 3
+			for (int startR = 0; startR < 9 && isValid; startR += 3) {
+				for (int startC = 0; startC < 9 && isValid; startC += 3) {
+					Set<Integer> block = new HashSet<>();
+					for (int r = startR; r < startR + 3; r++) {
+						for (int c = startC; c < startC + 3; c++) {
+							int num = map[r][c];
+							if (!block.add(num)) {
+								isValid = false;
+								break;
+							}
+						}
+						if(!isValid) break;
+					}
+				}
+			}
+			System.out.println("#" + tc + " " + (isValid ? 1 : 0));
 		}
 	}
 }
