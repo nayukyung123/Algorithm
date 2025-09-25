1adbc28e5e07d8347437937e50ddd0489fb5346c
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-25 16:10:11 +0900

    [Gold V] Title: 센서, Time: 184 ms, Memory: 16408 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -2,16 +2,14 @@ import java.io.*;
 import java.util.*;
 
 //어떻게 K로 쪼갠 배열의 합이 최소값이 될 수 있을까
-//1. 배열(arr)을 입력받기
-//2. arr를 정렬해서 
-//3. (diff)
+
 public class Main {
 
 	static int[] arr;
 	static int N;
 	static int K;
 	static int sum;
-	static Integer[] diff; // 센서 거리 값들 들어가있음
+	static Integer[] diff; // 내림차순 정렬을 위해 Integer[]사용, 센서 거리 값들 들어가있음
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,12 +26,12 @@ public class Main {
 	
 		Arrays.sort(arr);// 1 3 6 6 7 9
 
-		diff = new Integer[N - 1];
+		diff = new Integer[N - 1]; // 
 		for (int i = 0; i < N - 1; i++) {
-			diff[i] = arr[i + 1] - arr[i];
+			diff[i] = arr[i + 1] - arr[i]; // [2, 3, 0, 1, 2]
 		}
 
-		Arrays.sort(diff, Collections.reverseOrder()); // [2, 3, 0, 1, 2]
+		Arrays.sort(diff, Collections.reverseOrder()); // [0, 1, 2, 2, 3]
 
 		sum = 0;
 		for (int i = K - 1; i < diff.length; i++) { //K - 1 : 가장 큰 간격 버리기 위해 
