15f78dace0f87f677b40799c6d1ed49d2b08a8f0
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-28 13:01:30 +0900

    [D4] Title: 치즈 도둑, Time: 302 ms, Memory: 46,516 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -1,72 +1,74 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Solution {
 
-	static int[][] cheese; // 치즈
-	static int N; // 한변의 길이
-	static int max; // 치즈 최대 갯수
-	static int cheeseCnt; // 실시간 치즈 카운트
+	static int N;
+	static int cnt;
+	static int maxCnt;
+	static int[][] cheese;
+	static boolean[][] visited;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
-	static boolean[][] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int T = Integer.parseInt(br.readLine());
+
 		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
 
-			N = Integer.parseInt(br.readLine()); // 한변의 길이 입력받음
-
-			cheese = new int[N][N]; // 치즈 배열 초기화
-			int maxCheese = 0; // 입력 치즈 값 중 최대값
+			cheese = new int[N][N];
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					cheese[i][j] = Integer.parseInt(st.nextToken());
-					maxCheese = Math.max(maxCheese, cheese[i][j]);
 				}
-			} // 치즈 완성
-
-			// ->0 ~ maxCheese일 진행
-			max = 0;
-			for (int day = 0; day <= maxCheese; day++) {
+			}
+			
+			maxCnt = 0;
+			for (int day = 0; day < 100; day++) {
 				visited = new boolean[N][N];
-				cheeseCnt = 0; // 하루 마다 덩어리 세야하니까 day 반복문 에서 초기화
+				cnt = 0;
 
+				
 				for (int i = 0; i < N; i++) {
 					for (int j = 0; j < N; j++) {
 						if (day == cheese[i][j]) {
 							cheese[i][j] = 0;
 						}
 					}
-				} // 치즈 먹기
+				}// 치즈먹기
 
+				
 				for (int i = 0; i < cheese.length; i++) {
 					for (int j = 0; j < cheese.length; j++) {
 						if (!visited[i][j] && cheese[i][j] != 0) {
 							dfs(i, j);
-							cheeseCnt++;
+							cnt++;
 						}
 					}
-				} // 치즈 덩어리 세기
-				max = Math.max(max, cheeseCnt); // 치즈 덩어리 최대값 갱신
+				}// 치즈덩어리세기
+				
+				maxCnt = Math.max(maxCnt, cnt); // 치즈 덩어리 최대값 갱신 
 			}
-			System.out.println("#" + tc + " " + max);
+			System.out.println("#" + tc + " " + maxCnt);
 		}
 	}
 
-	private static void dfs(int r, int c) {
-		visited[r][c] = true;
-
+	private static void dfs(int i, int j) {
+		visited[i][j] = true;
+		
 		for (int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+			int nr = i + dr[d];
+			int nc = j + dc[d];
 
-			if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc] && cheese[nr][nc] != 0) {
+			if (nr >= 0 && nc >= 0 && nr < N && nc < N && !visited[nr][nc] && cheese[nr][nc] != 0) {
+				visited[nr][nc] = true;
 				dfs(nr, nc);
 			}
+
 		}
 	}
 }
