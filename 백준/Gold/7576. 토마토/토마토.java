1b809af0adc9156e04fd8333b5f4cd38b13428a3
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-28 16:45:09 +0900

    [Gold V] Title: 토마토, Time: 544 ms, Memory: 119916 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,20 +1,22 @@
 import java.io.*;
 import java.util.*;
 
+//토마토 
 public class Main {
 
-	static int N, M;
+	static int M, N, ans;
 	static int[][] box;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 
-		box = new int[N][M];
+		box = new int[N][M]; // 박스초기화
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
@@ -22,55 +24,54 @@ public class Main {
 			}
 		}
 
-		bfs();
 
-		int result = 0;
+		bfs();// 토마토익히기
+
+		// 토마토익히고 박스확인
+		ans = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (box[i][j] == 0) {
-					System.out.println(-1);
+				if (box[i][j] == 0) { // 익지않은게 있다면
+					ans = -1;
+					System.out.println(ans);
 					return;
 				}
-				result = Math.max(result, box[i][j]);
+				ans = Math.max(ans, box[i][j]);
 			}
 
 		}
-		if (result == 1) {
-			System.out.println(0);
-		} else {
-			System.out.println(result - 1);
-		}
+		System.out.println(ans - 1);
 
 	}
 
 	private static void bfs() {
+		// 익은 토마토를 전부 큐에 넣음
 		Queue<int[]> q = new LinkedList<>();
-
-		// 시작점 여러 개, 모든 1을 동시에 시작점으로 하기 
-		for (int k = 0; k < N; k++) {
-			for (int l = 0; l < M; l++) {
-				if (box[k][l] == 1) {
-					q.add(new int[] { k,l });
-				}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (box[i][j] == 1)
+					q.offer(new int[] { i, j });
 			}
 		}
 
+		// 익은 토마토들이 사방에서 익는 중
 		while (!q.isEmpty()) {
 			int[] cur = q.poll();
-			int r = cur[0];
-			int c = cur[1];
+			int sr = cur[0];
+			int sc = cur[1];
 
 			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if (nr >= 0 && nr < N && nc >= 0 && nc < M && box[nr][nc] == 0) {
-					box[nr][nc] = box[r][c] + 1;
-					q.add(new int[] { nr, nc });
+				int nr = sr + dr[d];
+				int nc = sc + dc[d];
 
+				// 익어있지않다면
+				if (nr >= 0 && nc >= 0 && nr < N && nc < M && box[nr][nc] == 0) {
+					box[nr][nc] = box[sr][sc] + 1;
+					q.offer(new int[] { nr, nc });
 				}
 			}
-
 		}
+
 	}
+
 }
