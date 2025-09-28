380025c955718080147145e9d68d2d81e3803caa
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-09-28 12:01:05 +0900

    [D3] Title: N-Queen, Time: 93 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,55 +1,52 @@
-import java.util.*;
 import java.io.*;
-import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Solution {
 
 	static int N;
-	static int count;
-	static int[] arr;
+	static int ans;
+	static boolean[] usedRow;
+	static boolean[] usedDiag1;
+	static boolean[] usedDiag2;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
-			arr = new int[N];
 
-			count = 0;
-			nQueen(0);
+			usedRow = new boolean[N];
+			usedDiag1 = new boolean[2 * N - 1];
+			usedDiag2 = new boolean[2 * N - 1];
 
-			System.out.println("#" + tc + " " + count);
-
+			ans = 0;
+			dfs(0);
+			System.out.println("#" + tc + " " + ans);
 		}
 	}
 
-	private static void nQueen(int row) {
-		if (row == N) {
-			count++;
+	private static void dfs(int col) {
+		if (col == N) {
+			ans++;
 			return;
 		}
-
-		for (int col = 0; col < N; col++) {
-			arr[row] = col;
-
-			if (possible(row))
-				nQueen(row + 1);
+		for (int row = 0; row < N; row++) {
+			if (usedRow[row] || usedDiag1[row + col] || usedDiag2[col - row + N - 1])
+				continue;
+			
+			usedRow[row] = true;
+			usedDiag1[row + col] = true;
+			usedDiag2[col - row + N - 1]=true;
+			
+			dfs(col+1);
+			
+			usedRow[row] = false;
+			usedDiag1[row + col] = false;
+			usedDiag2[col - row + N - 1]=false;
+			
 		}
-
-	}
-
-	private static boolean possible(int row) {
-		for (int i = 0; i < row; i++) {
-			if (arr[i] == arr[row]) {
-				return false;
-			}
-			if (Math.abs(row - i) == Math.abs(arr[row] - arr[i])) {
-				return false;
-			}
-		}
-		return true;
-
 	}
 
 }
