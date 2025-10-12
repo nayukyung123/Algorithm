e44b11216037bed6253d9f230f1f22ae354fc657
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-10-12 15:23:05 +0900

    [Silver II] Title: 좌표 압축, Time: 1768 ms, Memory: 265276 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+//“좌표를 압축한다” -> ‘원래 숫자 대신 그 숫자의 순서(인덱스) 를 쓰는 것’ 
+//입력: 2 4 -10 4 -9
+//출력: 2 3 0 3 1
+public class Main {
+
+	static int N;
+	static int[] arr;
+	static int[] clone;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		clone = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			clone[i] = arr[i]; // 원래 입력 순서 기억용
+		}
+		// 복사본 정렬 (작은 수부터)
+		Arrays.sort(clone);
+
+		// Map : 값에 인덱스 매핑
+		Map<Integer, Integer> map = new HashMap<>();
+		int idx = 0;
+		for (int num : clone) {
+			if (!map.containsKey(num))
+				map.put(num, idx++);
+		}
+
+		// 출력
+		StringBuilder sb = new StringBuilder();
+		for (int n : arr) {
+			sb.append(map.get(n)).append(" ");
+		}
+		System.out.println(sb.toString());
+
+	}
+}
