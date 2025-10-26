515a1a5977b09a52dd253a61c453d49ceb7f4399
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-10-26 09:56:35 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 78 ms, Memory: 24,832 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,27 +1,44 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Solution {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
+
 		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			Set<Integer> set = new HashSet<>();
-			
-			int cnt = 0;
-			int result = 0;
-			
-			while (set.size()<10) {
-				 result = N * ++cnt; // int형 한 글자 씩 빼오는 법 
-				 String s = String.valueOf(result); // int -> String 
-				 for (char c : s.toCharArray()) { // toCharArray 문자열을 문자 하나씩 잘라서 배열에 담아줌
-					set.add(c -'0');
+			int N = Integer.parseInt(br.readLine());
+
+			int[] arr = new int[9];
+			for (int i = 0; i < 9; i++) {
+				arr[i] = N * (i + 1);
+			} // N * ( 1 ~ 9 )
+
+			boolean[] seen = new boolean[10];// 0 ~ 9 중복 체크 
+			int cnt = 0;// 곱한개수 ( 출력 ) 
+			int cntseen = 0;// 0 ~ 9 중 본 숫자의 개수 (10개)
+
+			while (cntseen < 10) {
+				cnt++;
+				int num = N * cnt;
+				String str = String.valueOf(num); // 10 -> "10"
+				for (char c : str.toCharArray()) { // "10" -> "1", "0"
+					int digit = c - '0'; // "10" -> 10
+					if (!seen[digit]) {
+						seen[digit] = true;
+						cntseen++;
+					}
 				}
+
 			}
-			System.out.println("#" + tc + " " + result);
+
+			System.out.println("#" + tc + " " + N * cnt);
 		}
 	}
+
 }
