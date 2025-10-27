d92013396c347f3965667ebf8c475f86203e0d18
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-10-27 14:05:01 +0900

    [D2] Title: 파리퇴치3, Time: 79 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
@@ -1,81 +1,74 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Solution {
-    //가로, 세로 방향
-    static int [] dr1 = {-1, 1, 0, 0};
-    static int [] dc1 = {0, 0, -1, 1};
-     
-    //대각선 방향
-    static int [] dr2 = {-1, -1, 1, 1};
-    static int [] dc2 = {-1, 1, -1, 1};
-     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         
-        int t = sc.nextInt();
-        //테스트케이스 시작
-        for(int tc=1; tc<=t; tc++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-             
-            //새로운 배열 생성
-            int [][] arr = new int [n][n];
-             
-             
-             
-            //배열 입력
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-             
-            System.out.print("#" + tc + " ");
-             
-            //갱신할 최대값
-            int max = 0;
-             
-            //가로, 세로 합 계산
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    int sum1 = arr[i][j];
-                     
-                    for(int dir=0; dir<4; dir++) {
-                        for(int l=1; l<m; l++) {
-                            int nr = i + dr1[dir] * l;
-                            int nc = j + dc1[dir] * l;
-                             
-                            if(nr >= 0 && nr < n && nc >= 0 && nc < n) {
-                                sum1 += arr[nr][nc];
-                            }
-                        }
-                    }                  
-                    //현 위치 변동 때마다 합계 비교
-                    max = Math.max(max, sum1);
-                }
-            }
-             
-            //대각선 계산
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    int sum2 = arr[i][j];
-                     
-                    for(int dir=0; dir<4; dir++) {
-                        for(int l=1; l<m; l++) {
-                            int nr = i + dr2[dir] * l;
-                            int nc = j + dc2[dir] * l;
-                             
-                            if(nr >= 0 && nr < n && nc >= 0 && nc < n) {
-                                sum2 += arr[nr][nc];
-                            }
-                        }
-                    }
-                    //현 위치 변동 때마다 합계 비교
-                    max = Math.max(max, sum2);
-                }
-            }
-            System.out.println(max);
-        }
-    }
+
+	static int[] drPlus = { -1, 1, 0, 0 };
+	static int[] dcPlus = { 0, 0, -1, 1 };
+	static int[] drX = { -1, -1, 1, 1 };
+	static int[] dcX = { -1, 1, -1, 1 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			int[][] map = new int[N][N];
+			for (int i = 0; i < map.length; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < map.length; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			int sumPlus = 0;
+			int sumX = 0;
+			int max = 0;
+
+			for (int r = 0; r < map.length; r++) {
+				for (int c = 0; c < map.length; c++) {
+					sumPlus = map[r][c];
+					sumX = map[r][c];
+
+					// + 모양
+					for (int d = 0; d < 4; d++) {
+						for (int m = 1; m < M; m++) {
+							int nr = r + drPlus[d] * m;
+							int nc = c + dcPlus[d] * m;
+
+							if (nr >= 0 && nc >= 0 && nr < N && nc < N) {
+								sumPlus += map[nr][nc];
+							}
+
+						}
+
+					}
+
+					// x 모양
+					for (int d = 0; d < 4; d++) {
+						for (int m = 1; m < M; m++) {
+							int nr = r + drX[d] * m;
+							int nc = c + dcX[d] * m;
+
+							if (nr >= 0 && nc >= 0 && nr < N && nc < N) {
+								sumX += map[nr][nc];
+							}
+
+						}
+					}
+
+					max = Math.max(max, Math.max(sumPlus, sumX));
+
+				}
+			}
+			System.out.println("#" + tc + " " + max);
+
+		}
+	}
 }
- 
