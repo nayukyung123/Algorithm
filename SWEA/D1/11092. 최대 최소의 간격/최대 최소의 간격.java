584893afdfcfb275229124d05ea29444e203aed4
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-11-02 01:41:49 +0900

    [D1] Title: 최대 최소의 간격, Time: 76 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D1/11092. 최대 최소의 간격/최대 최소의 간격.java
+++ SWEA/D1/11092. 최대 최소의 간격/최대 최소의 간격.java
@@ -1,39 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			int[] arr = new int[N];
+			int n = Integer.parseInt(br.readLine());
 			
-			for (int i = 0; i < N; i++) {
-				arr[i] = sc.nextInt();
+			int[] arr = new int[n];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				arr[i]= Integer.parseInt(st.nextToken());
 			}
 			
-			int max = 0; // 최댓값 
-			int min =11; // 최솟값 
-			int maxidx = 0; // 최댓값 위치 
-			int minidx = 0; // 최솟값 위치 
+			int min = arr[0];
+			int max = arr[0];
+			int maxIdx = 1;
+			int minIdx = 1;
 			
-			for (int i = 0; i < N; i++) {
-				if (max <= arr[i]) {
+			for (int i = 1; i < arr.length; i++) {
+				if(arr[i] >= max) {
 					max = arr[i];
-					maxidx = i + 1;
+					maxIdx = i+1;
 				}
 				
-				if (min > arr[i]) {
+				if(arr[i] < min) {
 					min = arr[i];
-					minidx = i+1;
+					minIdx = i+1;
 				}
+				
 			}
-			int result = Math.abs(maxidx - minidx);
 			
-			System.out.println("#" + tc + " " +result);
-
+			System.out.println("#" + tc + " " + (Math.abs(maxIdx-minIdx))); 
+			
 		}
 	}
 }
