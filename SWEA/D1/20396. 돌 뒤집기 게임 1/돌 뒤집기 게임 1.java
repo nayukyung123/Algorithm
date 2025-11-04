20f3be938c078c354d0b22f33fd513717ceb634f
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-11-04 21:58:26 +0900

    [D1] Title: 돌 뒤집기 게임 1, Time: 84 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D1/20396. 돌 뒤집기 게임 1/돌 뒤집기 게임 1.java
+++ SWEA/D1/20396. 돌 뒤집기 게임 1/돌 뒤집기 게임 1.java
@@ -1,49 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
 
-	public static void main(String[] args) throws Exception {
+	static int[] dr = { 0, 1, 1, -1 };
+	static int[] dc = { 1, 0, 1, 1 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken()); // 돌의 초기상태
-			int m = Integer.parseInt(st.nextToken()); // 뒤집을 것들
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
 
 			int[] arr = new int[n];
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < arr.length; i++) {
-				arr[i] = Integer.parseInt(st.nextToken()); // 돌의 초기상태 채우기
+			for (int i = 0; i < n; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
 
-			for (int a = 0; a < m; a++) { // 뒤집을 번호(시작, 끝)
+			for (int k = 0; k < m; k++) {
 				st = new StringTokenizer(br.readLine());
-				int i = Integer.parseInt(st.nextToken());
-				int j = Integer.parseInt(st.nextToken());
+				int i = Integer.parseInt(st.nextToken()) - 1; // 시작 번호
+				int j = Integer.parseInt(st.nextToken()); // 이동 갯수
 
-				int start = i -1;
-				
-				int targetValue = arr[start];
-				
+				int copy = arr[i];// 저장해놓기
 
-				for (int k = 0; k < j; k++) {
-					int currIndex = start + k;
-					if(currIndex < n) {
-						arr[currIndex] = targetValue;
-					} else {
+				for (int p = 0; p < j; p++) { // 시작번호부터 이동갯수 까지
+					int curIdx = i + p;
+					if (curIdx < n) { // 범위제한 
+						arr[curIdx] = copy;
+					} else
 						break;
-					}
 				}
 
 			}
-			StringBuilder sb = new StringBuilder();
-			sb.append("#").append(tc);
 
-			for (int i = 0; i < n; i++) {
-				sb.append(" ").append(arr[i]);
+			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(tc).append(" ");
+			for (int i : arr) {
+				sb.append(i).append(" ");
 			}
 			System.out.println(sb.toString());
 
