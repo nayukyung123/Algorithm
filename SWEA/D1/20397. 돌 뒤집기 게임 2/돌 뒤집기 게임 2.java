eede24b81370f9d72ff238b1dd506c768228c147
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-11-04 16:18:13 +0900

    [D1] Title: 돌 뒤집기 게임 2, Time: 121 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D1/20397. 돌 뒤집기 게임 2/돌 뒤집기 게임 2.java
+++ SWEA/D1/20397. 돌 뒤집기 게임 2/돌 뒤집기 게임 2.java
@@ -1,57 +1,50 @@
+
 import java.util.Scanner;
 
 public class Solution {
 
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		for (int tc = 1; tc <= T; tc++) {
-			int n = sc.nextInt();
-			int m = sc.nextInt();
+        int T = sc.nextInt(); 
 
-			int[] arr = new int[n];
-			for (int i = 0; i < n; i++) { // 돌의 초기 배열 상태
-				arr[i] = sc.nextInt();
-			}
+        for (int tc = 1; tc <= T; tc++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt(); 
 
-			// 뒤집기 할 번호
-			for (int k = 0; k < m; k++) {
-				int i_center_pos = sc.nextInt(); 
-				int j_pairs = sc.nextInt(); 
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt(); 
+            }
 
-				int center = i_center_pos - 1;
+            
+            for (int i = 0; i < m; i++) {
+                int a = sc.nextInt() - 1; 
+                int b = sc.nextInt();
 
-				for (int p = 1; p <= j_pairs; p++) {
+               
+                for (int k = 1; k <= b; k++) {
+                    int left = a - k;  
+                    int right = a + k; 
 
-					// 마주보는 돌의 인덱스 계산
-					int left = center - p;
-					int right = center + p;
+                   
+                    if (left >= 0 && left < n && right >= 0 && right < n) {
+                        if (arr[left] == arr[right]) {  
+                            arr[left] = 1 - arr[left]; 
+                            arr[right] = 1 - arr[right];
+                        }
+                    }
+                }
+            }
 
-					if (left < 0 || right >= n) {
-						break;
-					}
+           
+            System.out.print("#" + tc + " ");
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println(); 
+        }
 
-					if (arr[left] == arr[right]) {
-						// 둘 다 뒤집기
-						arr[left] = 1 - arr[left];
-						arr[right] = 1 - arr[right];
-					}
-				}
-			}
-
-			// 1. (StringBuilder 삭제) 테스트 케이스 번호 먼저 출력 (줄바꿈 없이)
-			System.out.print("#" + tc); 
-
-			// 2. (StringBuilder 삭제) 배열을 순회하며 공백 + 값 출력
-			for (int next : arr) {
-				System.out.print(" " + next);
-			}
-			
-			// 3. (StringBuilder 삭제) 모든 출력이 끝나면 줄바꿈
-			System.out.println(); 
-		}
-		
-		sc.close();
-	}
+        sc.close(); 
+    }
 }
