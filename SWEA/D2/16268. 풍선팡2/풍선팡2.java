675764e4fdb13d539d3afc705fa52383f064c340
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2025-11-04 22:22:02 +0900

    [D2] Title: 풍선팡2, Time: 112 ms, Memory: 30,848 KB -BaekjoonHub

--- SWEA/D2/16268. 풍선팡2/풍선팡2.java
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.java
@@ -1,47 +1,50 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 
 		for (int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken()); // 가
-			int m = Integer.parseInt(st.nextToken()); // 세
+			int n = Integer.parseInt(st.nextToken()); // 가로 
+			int m = Integer.parseInt(st.nextToken()); // 세로 
 
-			int[][] map = new int[n][m];
+			int[][] arr = new int[n][m];
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < m; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			int max = Integer.MIN_VALUE;// 꽃가루 수 중 최대값
-
-			// 상하좌우더한값
-			for (int r = 0; r < n; r++) {
+			
+			int max = Integer.MIN_VALUE; // 꽃가루의최대값 
+			
+			
+			for (int r = 0; r < n; r++) { // 전체를 돈다 
 				for (int c = 0; c < m; c++) {
+					int sum = arr[r][c]; // 가운데  
 					
-					int sum = map[r][c];
-
-					for (int d = 0; d < 4; d++) {
+					for (int d = 0; d < 4; d++) { // 상하좌우  
 						int nr = r + dr[d];
 						int nc = c + dc[d];
-
-						if (nr >= 0 && nc >= 0 && nr < n && nc < m)
-							sum += map[nr][nc];
+						
+						
+						if(nr >=0 && nr < n && nc >=0 && nc <m) { // 범위제한 
+							sum += arr[nr][nc];
+						}
 					}
-					max = Math.max(max, sum);
+					max = Math.max(sum, max);
 				}
 			}
-			System.out.println("#" + tc + " " + max);
-
+			System.out.println("#" + tc + " " + max); 
 		}
 	}
 }
