22dd594b8cc5feb05b60b8a815867e8b7e5c51e4
nayukyung123 <141799443+nayukyung123@users.noreply.github.com>
2026-01-12 23:46:11 +0900

    [Gold V] Title: 회의실 배정, Time: 468 ms, Memory: 44656 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,40 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
 
-		int[][] meetings = new int[N][2]; // [시작, 끝]
+		int n = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++) {
+		int[][] meeting = new int[n][2];
+		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			meetings[i][0] = Integer.parseInt(st.nextToken()); // 시작 시간
-			meetings[i][1] = Integer.parseInt(st.nextToken()); // 끝 시간
+			meeting[i][0] = Integer.parseInt(st.nextToken());
+			meeting[i][1] = Integer.parseInt(st.nextToken());
 		}
 
-		// 끝나는 시간 기준 오름차순 정렬
-		// 끝나는 시간이 같으면 시작 시간 기준 정렬
-		Arrays.sort(meetings, (a, b) -> {
-			if (a[1] == b[1])
+		Arrays.sort(meeting, (a, b) -> {
+			if (a[1] == b[1]) {
 				return a[0] - b[0];
+			}
 			return a[1] - b[1];
 		});
 
-		int count = 0; // 회의 개수
-		int lastEnd = 0; // 마지막으로 배정된 회의의 끝 시간
+		int cnt = 0;
+		int lastEnd = 0;
 
-		for (int i = 0; i < N; i++) {
-			int start = meetings[i][0];
-			int end = meetings[i][1];
+		for (int i = 0; i < n; i++) {
+			int start = meeting[i][0];
+			int end = meeting[i][1];
 
-			if (start >= lastEnd) { // 이전 회의 끝나고 시작 가능하면
-				count++;
-				lastEnd = end; // 끝나는 시간 갱신
+			if (start >= lastEnd) {
+				cnt++;
+				lastEnd = end;
 			}
+
 		}
 
-		System.out.println(count);
+		System.out.println(cnt);
+
 	}
 }
